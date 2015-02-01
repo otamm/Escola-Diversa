765a7d5e5c5d93e84d63bc3de411a17bd3a56be6
otamm <oivatom@gmail.com>
2015-02-01 17:04:37 -0200

    Corrected

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    if logged_in? # 'current_user' only exists if the user is logged, so the variable is only defined that way.
-      @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
+    #if logged_in? # 'current_user' only exists if the user is logged, so the variable is only defined that way.
+    #  @micropost = current_user.microposts.build
+    #  @feed_items = current_user.feed.paginate(page: params[:page])
+    #end
   end
 
   def help
