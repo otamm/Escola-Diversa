64aeeb68ddd5176bb8460b73905331b57e8d7a2f
otamm <oivatom@gmail.com>
2015-02-02 06:15:55 -0200

    Removed carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,14 @@
-if Rails.env.production? # to be used only on production with Amazon web server.
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
+#if Rails.env.production? # to be used only on production with Amazon web server.
+#  CarrierWave.configure do |config|
+#    config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
-end
+#      :provider              => 'AWS',
+#      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+#      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+#    }
+#    config.fog_directory     =  ENV['S3_BUCKET']
+#  end
+#end
 
 # run this on terminal to set it up:
 #$ heroku config:set S3_ACCESS_KEY=<access key>
